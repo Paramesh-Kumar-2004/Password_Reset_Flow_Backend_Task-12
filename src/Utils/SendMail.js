afffc86b4556084ac9ad0815d920862ee925d9f7
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-12-08 14:20:51 +0530

    Change The service gmail to Gmail Still Not Solved

--- src/Utils/SendMail.js
+++ src/Utils/SendMail.js
@@ -39,26 +39,26 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-    service: "Gmail",
-    auth: {
-        user: process.env.USER_EMAIL,
-        pass: process.env.PASS_KEY,
-    },
+  service: "Gmail",
+  auth: {
+    user: process.env.PASS_MAIL,
+    pass: process.env.PASS_KEY,
+  },
 });
 
 const sendEmail = async (to, subject, text) => {
-    const mailOptions = {
-        from: process.env.USER_EMAIL,
-        to,
-        subject,
-        text,
-    };
-    try {
-        await transporter.sendMail(mailOptions);
-        console.log("Email sent successfully");
-    } catch (error) {
-        console.error("Error sending email:", error);
-    }
+  const mailOptions = {
+    from: process.env.PASS_MAIL,
+    to,
+    subject,
+    text,
+  };
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log("Email sent successfully");
+  } catch (error) {
+    console.error("Error sending email:", error);
+  }
 };
 
 export default sendEmail;
