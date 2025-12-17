2391ce3f022d3220c5e308dc81b3ff70ddfeb651
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-12-17 22:35:48 +0530

    Changing The Mail Send Option Nodemailer To Resend

--- src/Utils/SendMail.js
+++ src/Utils/SendMail.js
@@ -4,31 +4,63 @@ dotenv.config();
 
 
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.PASS_MAIL,
-    pass: process.env.PASS_KEY,
-  },
-});
+// const transporter = nodemailer.createTransport({
+//   host: "smtp.gmail.com",
+//   port: 587,
+//   secure: false,
+//   auth: {
+//     user: process.env.PASS_MAIL,
+//     pass: process.env.PASS_KEY,
+//   },
+// });
 
+
+// const sendMail = async (to, subject, text) => {
+//   const mailOptions = {
+//     from: process.env.PASS_MAIL,
+//     to,
+//     subject,
+//     text,
+//   };
+//   try {
+//     return await transporter.sendMail(mailOptions)
+//       .then(() => console.log("MAIL SENT"))
+//       .catch(err => console.log("MAIL ERROR:", err));
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// export default sendMail;
+
+
+
+
+
+
+
+
+
+
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendMail = async (to, subject, text) => {
   const mailOptions = {
-    from: process.env.PASS_MAIL,
+    from: "onboarding@resend.dev", // must be verified in Resend
     to,
     subject,
-    text,
+    text
   };
+
   try {
-    return await transporter.sendMail(mailOptions)
+    return await resend.emails.send(mailOptions)
       .then(() => console.log("MAIL SENT"))
       .catch(err => console.log("MAIL ERROR:", err));
   } catch (error) {
     console.log(error);
   }
-}
+};
 
 export default sendMail;
